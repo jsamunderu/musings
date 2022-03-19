639723cc3f8341e9d2c65d5ff5dfb31581666977
James Samunderu <jsamunderu@gmail.com>
2022-03-19 16:28:23 +0200

    update

--- python/poly.py
+++ python/poly.py
@@ -5,8 +5,14 @@ def polynomial(coef, x):
         y = y * x + coef[i]
     return y
 
+def polynomial_recursive(coef, x, n):
+    if n == (len(coef)-1):
+        return coef[n]
+    return polynomial_recursive(coef, x, n+1) * x + coef[n]
+
 if __name__ == '__main__':
     # evaluate 2x^2 + 5x + 7 where x = 13
     coef = [7, 5, 2]
     x = 13
     print(polynomial(coef, x))
+    print(polynomial_recursive(coef, x, 0))
